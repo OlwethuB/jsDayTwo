cd8f445c64f4645bf173962f33a43f389ce14d7b
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-05-16 15:49:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let name = "Olwethu";
+let surname = "Bhiya";
+let numberVar = 5
+
+console.log( numberVar*5)
+
+const onename ='Olwethu'
+const onename ='Bhiya'
+
+let fullName = name + ' ' + surname
+
+console.log (surname)
+console.log (numberVar* 5)
+console.log ('Hello ' + fullName)
+
+
+//  Day 3
+
+ 
+let please = 'because I said so... ';
+
+console.log(typeof 'please')
+
+console.log(typeof 5)
+
+let bro = 'No, ';
+
+console.log( bro + please)
+
+let no = true;
+let yes = false;
+
+console.log(typeof no)
+
+
+// Day 4
+
